11ca61416b147b386e864b180f0dcc99509057e3
White-Chen <q953387601@163.com>
2016-12-11 22:06:47 +0800

    Chapter 3

---
+++ Thinking-In-Java/chapter3/exercises/Exercise_1.java
@@ -0,0 +1,21 @@
+
+import java.util.Date;
+import static net.mindview.util.Print.print;
+
+/**
+ * Created by ChenZhe on 12/11/2016.
+ * @author Chen Zhe
+ * @author q953387601@163.com
+ * @version 1.0.0
+ */
+public class Exercise_1 {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        print(new Date());
+        System.out.println(new Date());
+    }
+}
