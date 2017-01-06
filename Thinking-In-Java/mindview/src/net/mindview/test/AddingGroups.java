965d97390bac3a7f5fb6c9f1c7bdb119ddeec119
ChenZhe <q953387601@163.com>
2017-01-06 16:23:42 +0800

    Chapter 11

--- Thinking-In-Java/mindview/src/net/mindview/test/AddingGroups.java
+++ Thinking-In-Java/mindview/src/net/mindview/test/AddingGroups.java
@@ -1,5 +1,7 @@
 package net.mindview.test;
 
+import java.util.*;
+
 /**
  * Created by ChenZhePC on 2017/1/6.
  */
@@ -11,4 +13,15 @@ class Heavy extends Powder{}
 class Crusty extends Snow{}
 class Slush extends Snow{}
 public class AddingGroups {
+    public static void main(String[] args) {
+        Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+        Integer[] moreInts = {6, 7, 8, 9, 10};
+        collection.addAll(Arrays.asList(moreInts));
+        Collections.addAll(collection, 11, 12, 13, 14, 15);
+        Collections.addAll(collection, moreInts);
+
+        List<Integer> list = Arrays.asList(16, 17, 18, 19, 20);
+        list.set(1, 21);
+        list.add(21);
+    }
 }
