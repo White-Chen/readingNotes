36f3dab82009fbaea2266670f68e0c198c51176b
ChenZhe <q953387601@163.com>
2017-01-14 12:20:24 +0800

    Chapter 13

--- Thinking-In-Java/mindview/src/net/mindview/test/chapter13/Concatenation.java
+++ Thinking-In-Java/mindview/src/net/mindview/test/chapter13/Concatenation.java
@@ -4,5 +4,9 @@ package net.mindview.test.chapter13;
  * Created by ChenZhePC on 2017/1/14.
  */
 public class Concatenation {
-
+    public static void main(String[] args) {
+        String mango = "mango";
+        String s = "abc" + mango + "def" + 47;
+        System.out.println(s);
+    }
 }
