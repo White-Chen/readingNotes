b4d63fc1d891bbad62e95520ffb8740afcf15674
White-Chen <q953387601@163.com>
2017-01-15 22:22:34 +0800

    Chapter 5. Create RootConfig class and WebConfig class.

---
+++ Spring-In-Action/chapter5/src/main/java/spittr/config/WebConfig.java
@@ -0,0 +1,12 @@
+package spittr.config;
+
+/**
+ * \* Created with Chen Zhe on 1/15/2017.
+ * \* Description:
+ * \* @author ChenZhe
+ * \* @author q953387601@163.com
+ * \* @version 1.0.0
+ * \
+ */
+public class WebConfig {
+}
