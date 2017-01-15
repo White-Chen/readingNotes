0ea2a31a98fb8cf5c66ee8557a8b022add90a4ef
White-Chen <q953387601@163.com>
2017-01-15 22:27:40 +0800

    Chapter 5. Config DispatcherServlet.

--- Spring-In-Action/chapter5/src/main/java/spittr/config/SpittrWebAppInitializer.java
+++ Spring-In-Action/chapter5/src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -1,12 +1,40 @@
 package spittr.config;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * \* Created with Chen Zhe on 1/15/2017.
- * \* Description:
+ * \* Description: 负责将请求路由到其他的组件中
  * \* @author ChenZhe
  * \* @author q953387601@163.com
  * \* @version 1.0.0
  * \
  */
-public class SpittrWebAppInitializer {
+public class SpittrWebAppInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    /**
+     * 配置RootConfig类的位置
+     */
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { RootConfig.class};
+    }
+
+    /**
+     * 配置WebConfig类的位置
+     */
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class};
+    }
+
+    /**
+     * 将一个或多个路径映射到DispatcherServlet上
+     * 这里通过 “/” 声明它会是默认Servlet，他会处理进入应用的所有请求。
+     */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
 }
