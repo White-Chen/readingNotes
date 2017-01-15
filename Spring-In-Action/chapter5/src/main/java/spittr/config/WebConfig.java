8720c0aa8a44f03319f43462fa22569a2762d57f
White-Chen <q953387601@163.com>
2017-01-15 23:10:01 +0800

    Chapter 5. Override WebMvcConfigurerAdapter.

--- Spring-In-Action/chapter5/src/main/java/spittr/config/WebConfig.java
+++ Spring-In-Action/chapter5/src/main/java/spittr/config/WebConfig.java
@@ -1,12 +1,42 @@
 package spittr.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 /**
  * \* Created with Chen Zhe on 1/15/2017.
  * \* Description:
+ *      DispatcherServlet会加载当前类中@Configuration注解的bean
  * \* @author ChenZhe
  * \* @author q953387601@163.com
  * \* @version 1.0.0
  * \
  */
-public class WebConfig {
+
+@Configuration                  //声明当前Java类为Spring配置类
+@EnableWebMvc                   // 启用Spring MVC
+@ComponentScan("spittr.web")    //配置扫描路径
+public class WebConfig
+        extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
