dae6aeea342330c4f6834ce1ddc88515297897f9
White-Chen <q953387601@163.com>
2017-01-20 21:07:45 +0800

    Chapter 7. Learn the redirect by Flash.

--- Spring-In-Action/chapter7/src/main/java/spittr/web/SpitterController.java
+++ Spring-In-Action/chapter7/src/main/java/spittr/web/SpitterController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import spittr.data.SpitterRepository;
 import spittr.entity.Spitter;
 
@@ -39,23 +40,28 @@ public class SpitterController {
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public String processRegistration(
             @Valid SpitterForm spitterForm,
-            Errors errors) throws IOException {
+            Errors errors, RedirectAttributes model) throws IOException {
         if (errors.hasErrors())
             return "registerForm";
-        spitterRepository.save(spitterForm.toSpitter());
+        Spitter spitter = spitterForm.toSpitter();
+        spitterRepository.save(spitter);
         spitterForm
                 .getProfilePicture()
                 .transferTo(
-                        new File("C:/Users/ChenZhe/Desktop/temp" + spitterForm.getUsername() + ".jpg"));
-        return "redirect:/spitter/" + spitterForm.getUsername();
+                        new File("C:/Users/ChenZhe/Desktop/temp" + spitter.getUsername() + ".jpg"));
+        model.addAttribute("username",spitter.getUsername());
+        model.addFlashAttribute(spitter);
+        return "redirect:/spitter/{username}";
     }
 
     @RequestMapping(value = "/{username}", method = RequestMethod.GET)
     public String showSpitterProfile(
             @PathVariable String username,
             Model model){
-        Spitter spitter = spitterRepository.findByUsername(username);
-        model.addAttribute(spitter);
+        if (!model.containsAttribute("spitter")){
+            model.addAttribute(
+                    spitterRepository.findByUsername(username));
+        }
         return "profile";
     }
 }
